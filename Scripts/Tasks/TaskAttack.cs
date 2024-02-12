2e93e6ec75a83daae2da9657a97bd2a5dc1fb732
GhostTales <120366210+GhostTales@users.noreply.github.com>
2024-02-12 13:27:49 +0100

    player stats and enemy damage plus player attackarea

--- Scripts/Tasks/TaskAttack.cs
+++ Scripts/Tasks/TaskAttack.cs
@@ -1,5 +1,6 @@
 using BehaviorTree;
 using Godot;
+using playerstats;
 using System;
 
 public partial class TaskAttack : BtNode
@@ -8,12 +9,13 @@ public partial class TaskAttack : BtNode
 	Node2D player;
 	Node2D transform;
 	Timer Timer;
-	public TaskAttack(Node2D transform, Node2D player, Timer Timer)
+	int Damage;
+	public TaskAttack(Node2D transform, Node2D player, Timer Timer, int Damage)
 	{
-		GD.Print(Timer);
 		this.player = player;
 		this.transform = transform;
 		this.Timer = Timer;
+		this.Damage = Damage;
 		this.Timer.OneShot = false;
 		this.Timer.Timeout += timeout;
 		this.Timer.WaitTime = 1;
@@ -24,7 +26,12 @@ public partial class TaskAttack : BtNode
 	{
 		if (transform.Position.DistanceTo(player.Position) <= 60)
 		{
-			GD.Print($"{transform} attacked {player}");
+			if (Damage - player_stats.Armour <= 0)
+				player_stats.Health -= 1;
+			else player_stats.Health -= Damage - player_stats.Armour;
+
+			GD.Print($"{transform} attacked {player} | player health: {player_stats.Health}/{player_stats.Max_Health}");
+
 		}
 	}
 	public override NodeState Evaluate()
---
+++ Scripts/player_stats.cs
@@ -0,0 +1,15 @@
+using System;
+using Godot;
+
+namespace playerstats
+{
+	public static class player_stats
+	{
+		public static Node2D Player_Id;
+		public static int Health; // current health
+		public static int Max_Health; // max health
+		public static int Health_Regen = 2; // passive health regen
+		public static int Damage = 15; // damage per hit
+		public static int Armour = 0; // damage reduction
+	}
+}
