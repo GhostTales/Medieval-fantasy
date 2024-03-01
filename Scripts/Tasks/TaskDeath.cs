2a39aa24e165cc7d40e9fb610508de17dedd8a3b
090105 <lnj090105@gmail.com>
2024-03-01 09:57:08 +0100

    Map collisions shape size

--- Scripts/Tasks/TaskDeath.cs
+++ Scripts/Tasks/TaskDeath.cs
@@ -4,19 +4,19 @@ using System;
 
 public partial class TaskDeath : BtNode
 {
-    // Called when the node enters the scene tree for the first time.
-    Node2D Enemy;
-    public TaskDeath(Node2D Enemy)
-    {
-        this.Enemy = Enemy;
-    }
+	// Called when the node enters the scene tree for the first time.
+	Node2D Enemy;
+	public TaskDeath(Node2D Enemy)
+	{
+		this.Enemy = Enemy;
+	}
 
-    public override NodeState Evaluate()
-    {
-        //GD.Print($"{Enemy} has been freed");
-        Enemy.GetParent().Free();
+	public override NodeState Evaluate()
+	{
+		//GD.Print($"{Enemy} has been freed");
+		Enemy.GetParent().Free();
 
-        return NodeState.RUNNING;
+		return NodeState.RUNNING;
 
-    }
+	}
 }
